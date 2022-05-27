e782b910ad5c12f836b8158db6a5541f7251fddf
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-05-27 07:57:07 -0500

    Time: 75 ms (77.02%), Space: 44.5 MB (53.94%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    const newArr=[];
+    
+    for (let i=0; i<s.length; i++){
+        newArr[indices[i]]=s[i]
+    }
+    return newArr.join('')
+};
