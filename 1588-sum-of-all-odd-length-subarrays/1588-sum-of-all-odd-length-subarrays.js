f1025de711ae641332a09de5fb42656a02eeb6f1
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-06-04 11:57:52 -0500

    Time: 151 ms (10.49%), Space: 47.6 MB (43.40%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -6,22 +6,14 @@ var sumOddLengthSubarrays = function(arr) {
     let counter=0;
     
     for(let i=0; i<arr.length;i++){
-        
-        for (let j=i+1; j<=arr.length ; j=j+2){
-          let miniArr= arr.slice(i,j);
-            console.log(miniArr)
-            let initialValue=counter;
-            
-            counter = miniArr.reduce(
+        for (let j=i+1; j<=arr.length ; j=j+2){ 
+            counter = arr.slice(i,j).reduce(
   (previousValue, currentValue) => previousValue + currentValue,
-  initialValue
+  counter
 );
         }
         
     }
     
-    
     return counter;
 };
-
-// .slice(start, end not including this index)
