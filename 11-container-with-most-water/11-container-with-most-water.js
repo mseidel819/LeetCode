862409ee0e493227cc57b59db87817eb2cc58dd1
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-06-22 07:48:29 -0500

    Time: 129 ms (24.73%), Space: 50 MB (25.12%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+       let maxArea = -1;
+    
+    let left = 0;
+    let right = height.length - 1;
+    
+    while (left < right) {
+        const lVal = height[left];
+        const rVal = height[right];
+        
+        const h = Math.min(lVal, rVal);
+        const w = right - left;
+        
+        maxArea = Math.max((h * w), maxArea);
+        
+        if (lVal > rVal) right--;
+        else left++;
+    }
+    
+    return maxArea
+};
