d6714f65b1715b24fa69fea3996c047480a5cfb9
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-07-07 07:53:41 -0500

    Time: 61 ms (96.15%), Space: 42.8 MB (93.95%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -4,11 +4,8 @@
  */
 var diagonalSum = function(mat) {
     let counter=0;
-    let primary=0;
-    let secondary=0;
     for(i=0;i<mat.length; i++){
-    counter+=mat[i][i];
-      counter+= mat[i][mat.length-1-i]  
+    counter+=mat[i][i]+ mat[i][mat.length-1-i];
    
         if(i===mat.length-1-i){
             counter-=mat[i][i];
