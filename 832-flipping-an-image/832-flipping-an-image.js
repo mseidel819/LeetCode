271c7403efb0f890b0ffe7fc2f5d48f71e3b3167
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-07-07 07:26:40 -0500

    Time: 133 ms (7.54%), Space: 43.7 MB (82.41%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.js
+++ 832-flipping-an-image/832-flipping-an-image.js
@@ -3,9 +3,5 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(image) {
-   const flip=image.map(arr=>arr.reverse());
-    
-    const invert=flip.map(arr=>arr.map(num=>num===0 ? num=1 :num=0))
-    
-    return invert
+   return image.map(arr=>arr.reverse().map(num=>num===0 ? num=1 :num=0));
 };
