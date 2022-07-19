e3fcedf5c0f9ea344abb907df1bba4d3686588de
Matt Seidel <91429736+mseidel819@users.noreply.github.com>
2022-07-19 07:28:58 -0500

    Time: 78 ms (100.00%), Space: 42.4 MB (100.00%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberOfPairs = function(nums) {
+    let pairCount=0;
+    const newArr=[];
+    
+    const sorted=nums.sort((a,b)=>{
+        return b-a
+    });
+    
+    let toggler=false;
+    for( let i=0;i<sorted.length;i++){
+//if we alreay counted as the second of a pair
+        if (toggler===true){
+            toggler=false;
+            
+        }else
+//if its a pair
+        if(sorted[i]===sorted[i+1]){
+            toggler=true;
+            pairCount+=1
+            
+        }else
+        
+    newArr.push(sorted[i]);
+        
+    }
+    const leftoverCount=newArr.length;
+    
+  return [pairCount, leftoverCount]
+};
